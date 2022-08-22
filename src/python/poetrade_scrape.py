f1253423bf7e7151552018589a05f106309b7ef4
wesh92 <52631646+wesh92@users.noreply.github.com>
2022-08-21 22:08:52 -0400

    transformations added :chart_with_upwards_trend:

--- src/python/poetrade_scrape.py
+++ src/python/poetrade_scrape.py
@@ -1,10 +1,14 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from typing import Dict, List
+from typing import Dict, List, Union
 import tomli
+import pendulum
+import os
 
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_columns', None)  
 
 def util_load_toml():
     rel_path = r'rmt-is-ok\src\python\configs\trusted_sellers.toml'
@@ -14,12 +18,11 @@ def util_load_toml():
 @dataclass
 class TradeData:
 
-    currency_map: Dict[str, str] = field(default_factory={
+    currency_map = {
         "6": "EXALT",
         "4": "CHAOS",
         "15": "DIVINE"
         }
-    )
     
     def _make_poetrade_urls_list(self, map_of_currency_to_id: Dict[str, str]
                                  = currency_map):
@@ -53,10 +56,9 @@ class TradeData:
                 df = df.append(df2, ignore_index=True)
         return df
 
-
+@dataclass
 class RMTData:
-    def __init__(self):
-        self.CONFIG = util_load_toml()
+    CONFIG = util_load_toml()
 
     @property
     def URLS(self):
@@ -66,8 +68,7 @@ class RMTData:
     def TRUSTED_SELLERS(self):
         return self.CONFIG['trusted_sellers']
 
-    def get_rmt_data(self):
-        columns = ['seller', 'currency', 'price']
+    def get_rmt_data(self, columns: List[str]):
         df = pd.DataFrame(columns=columns)
         for currency, url in self.URLS.items():
             sess = requests.session()
@@ -87,6 +88,68 @@ class RMTData:
                     pass
         return df
         
-        
-a = RMTData().get_rmt_data()
-print(a)
+
+class Transformations:
+    def __init__(self):
+        self.sell_columns = ['seller', 'recv_currency', 'recv_price']
+        self.pay_columns = ['seller', 'pay_currency', 'pay_price']
+        self.safe_limit: float = 300.0
+        self.keep_recv_currency: Union[List[str, str], str] = 'CHAOS'
+    
+    @property
+    def RECV_DATA(self):
+        return RMTData().get_rmt_data(self.sell_columns)
+    
+    @property
+    def PAY_DATA(self):
+        return RMTData().get_rmt_data(self.pay_columns)
+    
+    @property
+    def TRADE_DATA(self):
+        return TradeData().get_poetrade_data()
+    
+    # function to merge the two dataframes joined on the currency type
+    def merge_dataframes(self):
+        df = pd.merge(self.PAY_DATA[['pay_price', 'pay_currency']], self.TRADE_DATA, on='pay_currency')
+        df = pd.merge(df, self.RECV_DATA[['recv_price', 'recv_currency']], on='recv_currency')
+        return df
+    
+    # only select the first instance of each pay and recv currency pair
+    def select_first_instance(self):
+        df = self.merge_dataframes()
+        # remove anamlous rows > 200 recv currency
+        df = df[df['recv_price'].astype(float) <= self.safe_limit]
+        # only select rows where recv_currency is CHAOS
+        if isinstance(self.keep_recv_currency, str):
+            df = df[df['recv_currency'] == self.keep_recv_currency]
+        elif isinstance(self.keep_recv_currency, list):
+            df = df[df['recv_currency'].isin(self.keep_recv_currency)]
+        else:
+            raise ValueError('keep_recv_currency must be a list or string')
+        # only select rows where pay_value is 1
+        df = df[df['pay_value'].astype(float) <= 1] # the methodology here is a little skewed I know, but usually early rows are 1 chaos
+        df = df.drop_duplicates(subset=['pay_currency', 'recv_currency'], keep='first')
+        return df
+    
+    # math time, multiply the recv price by recv value.
+    # then multiply the pay price by pay value.
+    def create_prices_data(self):
+        ts = pendulum.now().strftime('%Y-%m-%d %H:%M:%S')
+        ts_short = pendulum.now().strftime('%Y-%m-%d-%H%M%S')
+        df = self.select_first_instance()
+        recv_price = df['recv_price'].astype(float)
+        recv_value = df['recv_value'].astype(float)
+        pay_price = df['pay_price'].astype(float)
+        pay_value = df['pay_value'].astype(float)
+        df['recv_cost'] = recv_price * recv_value
+        df['pay_cost'] = pay_price * pay_value
+        df['rmt_diff'] = round(df['recv_cost'], 2) - round(df['pay_cost'], 2)
+        df['rmt_diff_percent'] = df['rmt_diff'] / df['pay_cost']
+        df['timestamp'] = ts
+        # open a file in /src/data/ and write the dataframe to it
+        with open(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', f'rmt_data_{ts_short}.csv')), 'w+', newline='') as f:
+            df.to_csv(f, index=False)
+
+
+if __name__ == '__main__':
+    Transformations().create_prices_data()
